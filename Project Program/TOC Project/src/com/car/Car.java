cb3ca503d198121c3d577a47449ddcb0301264a4
CabTheProgrammer <55765253+CabTheProgrammer@users.noreply.github.com>
2021-02-26 17:49:47 -0500

    Finished text-based section of code

--- Project Program/TOC Project/src/com/car/Car.java
+++ Project Program/TOC Project/src/com/car/Car.java
@@ -13,10 +13,13 @@ import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 public class Car {
 
-	private int speed=0;
-	private String state, currentState, nextState;
+	static private int speed=0;
+	static private String state;
+	
+	private String currentState,nextState;
 	private String Stop;
-	private String signal,speechSignal;
+	private int signal;
+	private String speechSignal;
 	//Additions
 		
 	//  Setters and getters for speed and state
@@ -46,24 +49,76 @@ public class Car {
         System.out.println("\n" + a+"\n"+b);
     }
 	
-	
-	
-	
 	//End of Additions
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	//functions to reflect state
 	public void off() {
-		
-		state = "stationary";
-		
+		setState("OFF");
+		setSpeed(0);
 	}
+	
+	public void hold() {
+		setState("HOLD");
+		setSpeed(0);
+	}
+	
+	public void ignitionon() {
+		setState("IGNITION-ON");
+		setSpeed(0);
+	}
+	
+	 public void enginestarted()
+	    {
+		 setState("ENGINE-STARTED");
+		 setSpeed(0);
+	    }
+	 
+	 
+	public void stationary() {
+		setState("STATIONARY");
+        setSpeed(0);
+	}
+	
+	
+	public void inforwardmotion(int flag) {
+		//if flag is 1, we are coming from accelerate and can increment speed
+        //if flag is 0, then we can just set the speed to 1(coming from stationary)
+        //and if flag is -1 then we can decrement the speed and if the speed reaches zero, 
+        //transitions to the stationary state
+
+        setState("IN-FORWARD-MOTION");
+        if(flag == 1)
+            setSpeed(getSpeed()+1);
+        else if(flag==0)
+            setSpeed(1);
+        else if(flag == - 1)
+            setSpeed(getSpeed()-1);
+       
+        if(getSpeed()==0)
+        	stationary();
+	}
+	
+	 public void inreversemotion(int flag)
+	    {
+		 setState("IN-REVERSE-MOTION");
+		 	//if flag is 1, we are coming from the accelerate signal and can increment the speed
+	        //and if flag is -1 then we are coming from brake pressed and can decrement the speed and if the speed reaches zero, 
+	        //transitions to the stationary state
+		 
+		  if(flag == 1)
+	            setSpeed(getSpeed()+1);
+	        else if (flag==-1)
+	            setSpeed(getSpeed()-1);
+	        else
+	        	setSpeed(0); // Coming from the stationary position
+	    }
+	 
+	
+	 public void cruisecontrol() {
+		 setState("CRUISECONTROL");		 
+	 }
+	 
+	//end of functions that reflect state
 	
 	//Speech input
 	public void speech_signal() throws IOException {
@@ -112,36 +167,128 @@ public class Car {
 		
 	}
 		
-	
+	public void dispOptions() { //Reads state and displays appropriate options to user
+		System.out.println("Available Options");
+
+		switch(getState())
+		{
+		case "OFF":
+			System.out.println("1. Hold Brake");
+			System.out.println("2. Start Signal");
+			break;
+		case "HOLD":
+			System.out.println("1. Start Signal");
+			break;
+		case "IGNITION-ON":
+			System.out.println("1. Hold Brake");
+			System.out.println("2. Start Signal");
+			break;
+		case "ENGINE-STARTED":
+			System.out.println("1. Start Signal");
+			System.out.println("2. Engage Seatbelt");
+			break;
+		case "STATIONARY":
+			System.out.println("1. Accelerate");
+			System.out.println("2. Reverse");
+			System.out.println("3. Park");
+			System.out.println("4. Select Drive");
+			break;
+		case "IN-REVERSE-MOTION":
+			System.out.println("1. Accelerate");
+			System.out.println("2. Press Brake");
+			System.out.println("3. Hold Brake");
+			break;
+		case "IN-FORWARD-MOTION":
+			System.out.println("1. Accelerate");
+			System.out.println("2. Press Brake");
+			System.out.println("3. Activate Cruise Control");
+			break;
+		case "CRUISECONTROL":
+			System.out.println("1. Accelerate");
+			System.out.println("2. Press Brake");
+			break;
+		}
+			
+	}
 	
 	//Regular typing input
 	public void signal() {
 		try {
 			
-			Scanner in = new Scanner(System.in);
-		System.out.println("What signal are you giving? ");
-		signal = in.next();
-		
-		//HERE CAN USE IF STATEMENTS TO SEE WHERE THE CODE GOES FROM OFF STATE 
-		//TO MOVE THE CAR.
-		
-		//Gives a if statement to check what state it is in and if in relevant state and
-		// Is going to another relevant state then the speed changes.
-		if(state.toLowerCase().equals("stationary") && signal.toLowerCase().equals("drive")) {
-			
-			//IF it is correct, then the next state is given and the increase speed method then called.
-			increase_speed();
-			inForward_Motion();
-			
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to the RSC-based car model");
+			while(true) {
+				//Displays current state of the vehicle
+				printCurrentState();
+				//Display options based on state of the vehicle
+				dispOptions();
+				System.out.println("What signal are you giving? ");
+				signal = in.nextInt();
+				//HERE CAN USE IF STATEMENTS TO SEE WHERE THE CODE GOES FROM OFF STATE 
+				//TO MOVE THE CAR.
+				
+				//We are using a case to check what state it is in and if in relevant state and
+				// Is going to another relevant state then the speed changes.
+				
+				//Switch statement to handle state transition
+				switch(state) {
+				case "OFF":
+					if(signal==1)
+						hold();
+					else if(signal==2)
+						ignitionon();
+					break;
+				case "HOLD":
+					if(signal==1)
+						enginestarted();
+					break;
+				case "IGNITION-ON":
+					if(signal==1)
+						hold();
+					else if(signal==2)
+						off();
+					break;
+				case "ENGINE-STARTED":
+					if(signal==1)
+						off();
+					else if(signal==2)
+						stationary();
+					break;
+				case "STATIONARY":
+					if(signal==1)
+						stationary();
+					else if(signal==2)
+						inreversemotion(0);
+					else if(signal==3)
+						enginestarted();
+					else if(signal==4)
+						inforwardmotion(1);
+					break;
+				case "IN-REVERSE-MOTION":
+					if(signal==1)
+						inreversemotion(1);
+					else if(signal==2)
+						inreversemotion(-1);
+					else if(signal==3)
+						stationary();
+					break;
+				case "IN-FORWARD-MOTION":
+					if(signal==1)
+						inforwardmotion(1);
+					else if(signal==2)
+						inforwardmotion(-1);
+					else if(signal==3)
+						cruisecontrol();
+						break;
+				case "CRUISECONTROL":
+					if(signal==1)
+						cruisecontrol();
+					else if(signal==2)
+						inforwardmotion(22); // Any value other than 0,1 and -1 could have been placed here
+					break;
+				}
 		}
 		
-		if(state.toLowerCase().equals("stationary") && signal.toLowerCase().equals("reverse")) {
-			
-			//IF it is correct, then the next state is given and the increase speed method then called.
-			increase_speed();
-			inReverse_Motion();
-			
-		}
 		
 		}catch(NullPointerException e) {
 			System.out.println("Null pointer encountered at Signal method.");
@@ -149,43 +296,6 @@ public class Car {
 	}
 	
 	
-	public void inForward_Motion() {
-		
-		System.out.println("Gear Changed to drive + \n");
-		System.out.println("In-forward motion :) \n");
-		
-	}
-	
-	public void inReverse_Motion() {
-	
-		System.out.println("Gear Changed to reverse + \n");
-		System.out.println("In-reverse motion ;) \n");
-		
-	}
-	
-	public void increase_speed() {
-		
-		speed = 1;
-		System.out.println("\n Speed is "+speed);
-		
-	}
-	
-	public void decrease_speed() {
-		
-		speed = 0;
-		System.out.println("\n Speed is "+speed);
-		
-	}
-	
-	public void Stop() {
-		
-		
-		state = "Stationary";
-		decrease_speed();
-		
-		System.out.println("\n" +state);
-		
-	}
-	
+
 	
 }
